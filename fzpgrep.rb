8e942eb52380dd9bc182a4d0579a1733b5fdae94
Nikhil Kumar <nikhilkmr300@gmail.com>
2024-03-11 01:34:04 -0700

    Updated version in fzpgrep.rb

--- fzpgrep.rb
+++ fzpgrep.rb
@@ -3,7 +3,7 @@ class Fzpgrep < Formula
 
   desc "A fuzzy version of pgrep"
   homepage "https://github.com/nikhilkmr300/fzpgrep"
-  url "https://github.com/nikhilkmr300/fzpgrep/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/nikhilkmr300/fzpgrep/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "740dff128537172e5926f6075921eaa06f054e48be0fb831df0c7acf0e24a250"
 
   depends_on "python@3.11"
@@ -11,5 +11,9 @@ class Fzpgrep < Formula
   def install
     virtualenv_install_with_resources
     bin.install "src/fzpgrep"
+  end
+
+  test do
+    assert_match "fzpgrep", shell_output("#{bin}/fzpgrep --help")
   end
 end
