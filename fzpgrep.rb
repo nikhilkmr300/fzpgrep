5949b4578be7f3ce9d44ab8971a4bc1c7c9e27c6
Nikhil Kumar <nikhilkmr300@gmail.com>
2024-03-10 23:15:22 -0700

    Updated fzpgrep.rb

--- fzpgrep.rb
+++ fzpgrep.rb
@@ -2,7 +2,7 @@ class Fzpgrep < Formula
   include Language::Python::Virtualenv
 
   desc "A fuzzy version of pgrep"
-  homepage "https://github.com/yourusername/yourproject"
+  homepage "https://github.com/nikhilkmr300/fzpgrep"
   url "https://github.com/nikhilkmr300/fzpgrep/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "740dff128537172e5926f6075921eaa06f054e48be0fb831df0c7acf0e24a250"
 
@@ -10,5 +10,6 @@ class Fzpgrep < Formula
 
   def install
     virtualenv_install_with_resources
+    bin.install "src/fzpgrep"
   end
 end
