63802e8a3845e466297477a906cb53aa3193b7e5
Nikhil Kumar <nikhilkmr300@gmail.com>
2024-03-11 02:51:00 -0700

    Updated sha

--- fzpgrep.rb
+++ fzpgrep.rb
@@ -4,7 +4,7 @@ class Fzpgrep < Formula
   desc "A fuzzy version of pgrep"
   homepage "https://github.com/nikhilkmr300/fzpgrep"
   url "https://github.com/nikhilkmr300/fzpgrep/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "cacca0daf3d057e241c636fdbf454acab3b7e91ec529096eb9acfbc8efe24866"
+  sha256 "881f4f233d10a9d072770073dba7a89f27c94d8cbe2df805ad005da06e91d581"
 
   depends_on "python@3.11"
 
